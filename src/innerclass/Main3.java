ef402c34fbf9342600f96c14d29ab65a5bdbf55e
user <chl6698@naver.com>
2026-03-09 17:08:53 +0900

    oop2

---
+++ src/innerclass/Main3.java
@@ -0,0 +1,8 @@
+package innerclass;
+
+public class Main3 {
+    public static void main() {
+        OuterClass3 outerClass3 = new OuterClass3();
+        outerClass3.display();
+    }
+}
---
+++ src/innerclass/Spaceship.java
@@ -0,0 +1,34 @@
+package innerclass;
+
+//정적 내부 클래스 연습
+public class Spaceship {
+    private Engine engine;
+
+    //엔진을 추가한다
+    public void addEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public void startSpaceShip() {
+        if (engine == null) {
+            System.out.println("엔진을 먼저 장착해주세요");
+        } else {
+            System.out.println("우주여행을 출발 합니다");
+        }
+    }
+
+    //논리적으로 강하게 그룹화 시킬 때 내부 클래스를 사용할 수 있다
+    //정적 내부 클래스 Engine
+    public static class Engine {
+        private static int enginCount = 0;
+        private int serialNumber;
+
+        public Engine() {
+            this.serialNumber = ++enginCount;
+        }
+
+        public void start() {
+            System.out.println("Engine " + serialNumber + "동작합니다");
+        }
+    }
+}
